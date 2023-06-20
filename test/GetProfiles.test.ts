891cbde66b99afc86db88937eff3fdf49a2ff087
Salman Abid <salman-abid@hotmail.com>
2023-06-21 01:22:08 +0500

    feat: enter randomisation in tests

--- test/GetProfiles.test.ts
+++ test/GetProfiles.test.ts
@@ -1,6 +1,7 @@
 import "jest";
 import protoDescriptor from "../src/config/proto";
 import { credentials } from "@grpc/grpc-js";
+import { Result } from "../proto/profile/Result";
 
 const TRANSLATED_PORT = 50051;
 const DEATHSTAR_PORT = 8090;
@@ -16,36 +17,46 @@ describe("GetProfiles", () => {
       credentials.createInsecure()
     );
 
+    // array of randomly 5 generated hotel ids between 1 to 80
+    const hotelIds = [];
+
+    for (let i = 0; i < 5; i++) {
+      hotelIds.push(String(Math.floor(Math.random() * 80) + 1));
+    }
+
     const translated_promise = new Promise((resolve, reject) => {
-      client_translated.GetProfiles(
-        { hotelIds: ["1", "2"] },
-        (err, response) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(response);
-          }
+      client_translated.GetProfiles({ hotelIds: hotelIds }, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
         }
-      );
+      });
     });
 
     const original_promise = new Promise((resolve, reject) => {
-      client_original.GetProfiles(
-        { hotelIds: ["1", "2"] },
-        (err, response) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(response);
-          }
+      client_original.GetProfiles({ hotelIds: hotelIds }, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
         }
-      );
+      });
     });
 
-    const [translated_result, original_result] = await Promise.all([
+    const [translated_result , original_result] = await Promise.all([
       translated_promise,
       original_promise,
     ]);
+
+    // sort the hotels by id to make sure the order is the same
+    (translated_result as Result).hotels.sort((a, b) => {
+      return Number(a.id) - Number(b.id);
+    });
+
+    (original_result as Result).hotels.sort((a, b) => {
+      return Number(a.id) - Number(b.id);
+    });
 
     expect(translated_result).toEqual(original_result);
   });
