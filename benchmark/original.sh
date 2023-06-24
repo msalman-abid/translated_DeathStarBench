36b3a11c7d7791ec10868d5ba2d214fd22bdc05c
Salman Abid <salman-abid@hotmail.com>
2023-06-24 19:01:51 +0500

    chore: refactor benchmark script

--- benchmark/original.sh
+++ benchmark/original.sh
@@ -1,2 +1,7 @@
 # use ghz template function to generate random hotelIds and bench with 200 requests (default)
-ghz --insecure --proto ./proto/profile.proto --call profile.Profile.GetProfiles -d '{"hotelIds": ["{{randomInt 1 80}}"],"locale":"officia velit"}' 127.0.0.1:8090
+ghz --insecure \
+    --proto ./proto/profile.proto \
+    --call profile.Profile.GetProfiles \
+    -n 500 \
+    -d '{"hotelIds": ["{{randomInt 1 80}}"],"locale":"officia velit"}' \
+    127.0.0.1:8090
